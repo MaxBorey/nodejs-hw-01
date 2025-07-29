7af52c7eadb29a8cb94393a1990539f23f528529
maxborey <maxborey@gmail.com>
2025-07-29 16:42:35 +0300

    finish_

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -6,6 +6,6 @@ export const readContacts = async () => {
     const data = await fs.readFile(PATH_DB, 'utf-8');
     return JSON.parse(data);
   } catch (err) {
-    return [];
+    console.error('Помилка читання файлу:', err);
   }
 };
